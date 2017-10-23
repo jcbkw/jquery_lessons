83f759f487b8cde69776e10faa796f898d85782a
jcbcw <diannasinclar@aol.com>
2017-10-23 13:24:58 -0400

    Add click and fliter functionality to this component

--- js/dom_objects.js
+++ js/dom_objects.js
@@ -1,30 +1,39 @@
-function initialize (){
+export function initialize (callback){
 
         document.addEventListener("DOMContentLoaded", function () {
 
-            var button = document.getElementById("update").onclick = textChange;
-            console.log(button);
-            console.log("I am working");
-
+            callback();
 
         }, true);
         
 }
 
-function textChange() {
-debugger;
-    var inElement = document.getElementById("textIn");
-    var outElements = document.getElementsByTagName("p");
-    var headingElements = document.getElementsByClassName("heading");
+export function lesson1() {
 
-    for (let i = 0; i < outElements.length; i++) {
-
-        let element = outElements[i];
-        headingElements[i].innerHTML = "Updating" + (i+1) + "to" + inElement.value;
-        element.innerHTML = inElement.value;
-
+    function textChange() {
+    debugger;
+        var inElement = document.getElementById("textIn");
+        var outElements = document.getElementsByTagName("p");
+        var headingElements = document.getElementsByClassName("heading");
+    
+        for (let i = 0; i < outElements.length; i++) {
+    
+            let element = outElements[i];
+            headingElements[i].innerHTML = "Updating " + (i+1) + " to " + inElement.value;
+            element.innerHTML = inElement.value;
+    
+        }
+        
     }
+        function domHandlers() {
+    
+            var button = document.getElementById("update").onclick = textChange;
+            console.log(button);
+            console.log("I am working");
+    
+    }
+    
+    initialize(domHandlers);
 
-}
 
-initialize();
+};
---
+++ js/jquery_selectors.js
@@ -0,0 +1,74 @@
+import { initialize } from "./dom_objects.js";
+initialize(eventHandlerPageSetUp);
+
+
+function eventHandlerPageSetUp() {
+    $("body").on("click", function (e){
+        let target = e.target;
+        switch (target.id){
+            
+            case "even" : {setEven(); break} ;
+            case "odd"  : {setOdd(); break};
+            case "first": {setFirst4(); break}; 
+            
+            
+        }
+        console.log("odd is working");
+    });
+}
+
+// function formatter(elements) {
+    
+    
+    // }
+    
+    function setEven() {
+        //:Even Selector is counter intuitive as per JQuery API https://api.jquery.com/even-selector/
+        // Therefore I will use odd: to select even <li>
+        var $evenItems = $("li:odd");
+        var $elems = $("span:contains(Even)");
+        clear();
+        bold( [$elems, $evenItems]);
+        $(".label").html("Even");
+    }
+    function setOdd() {
+        
+        var $oddItems = $("li:even");
+        var $elems = $("span:contains(Odd)");
+        clear();
+        bold([$elems, $oddItems]);
+        $(".label").html("Odd");
+        
+    }
+    function setFirst4() {
+        
+        var $first4 = $("li:lt(4)");
+        var $elems = $("span:contains(First 4)");
+        clear();
+        bold([$elems, $first4]);
+        $(".label").html("Odd");
+        
+    }
+    
+    function clear() {
+        
+        $("li, span").each(function (index) {
+            
+            $(this).css("font-weight", "");
+        console.log(index)
+    }); 
+    
+}
+
+function bold(attrArrays) {
+    for (let i = 0; i < attrArrays.length; i++) {
+
+        $(attrArrays[i]).each(function(index){
+            
+                    $(this).css("font-weight", "bold");
+            
+        });
+
+    }
+
+}
